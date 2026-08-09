8a4b4de4e3b93200addeeb3bd2cd114b0aadc154
ChristianJavier <114371071+CHRIZNATENSE@users.noreply.github.com>
2026-08-09 12:57:58 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+
+    const message = document.getElementById("message");
+
+    message.innerHTML =
+        "Mom, thank you for everything you have done for us. ❤️<br><br>" +
+        "We love you so much! Happy Birthday! 🎂🌸";
+}
